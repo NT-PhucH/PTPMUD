3ada81265614a05e65b8bfef5dd1fa09d238e893
NT Phúc <Ntphuctb01@gmail.com>
2026-05-21 17:47:07 +0700

    update lần 3

---
+++ QLST/BLL (Bat ngoai le)/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLST.BLL__Bat_ngoai_le_
+{
+    internal class Class1
+    {
+    }
+}
