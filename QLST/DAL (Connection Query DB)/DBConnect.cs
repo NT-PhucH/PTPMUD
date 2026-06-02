bdd764875eda40caa921419e0bec2f8adbbcecef
Thangga205 <thangga21122005@gmail.com>
2026-06-02 18:16:41 +0700

    thiếu file connection file này để riêng ra không là bị lẫn lộn

---
+++ QLST/DAL (Connection Query DB)/DBConnect.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DBConnect
+    {
+        // Thay bằng Tên máy tính\Tên Instance SQL Server của bạn
+        public static string chuoiKetNoi = @"Data Source=.\SQLEXPRESS;Initial Catalog=QuanLySieuThi;Integrated Security=True";
+        protected SqlConnection conn;
+
+        public DBConnect()
+        {
+            conn = new SqlConnection(chuoiKetNoi);
+        }
+    }
+}
